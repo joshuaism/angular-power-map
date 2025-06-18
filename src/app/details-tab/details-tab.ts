05994d828105f93c63bce5468ba1c5f42caed9c4
joshuaism <joshuaism@msn.com>
2025-06-19 03:59:39 +0900

    improve entity details tab

--- src/app/details-tab/details-tab.ts
+++ src/app/details-tab/details-tab.ts
@@ -15,16 +15,34 @@ import { LittleSisNetwork } from '../network';
     </h1>
     <h4>{{ entity().blurb }}</h4>
     <div class="scrollable">
+      @if(entity().aliases && entity().aliases.length > 0) {
+      <h4>
+        aka: @for(alias of entity().aliases; track alias) {
+        <span>{{ alias }}</span>
+        @if(!$last) {<span>, </span>} }
+      </h4>
+      }
       <a href="{{ getFecUrl() }}" target="_blank">fec search</a>
       @if (entity().types[0].toUpperCase() !== 'PERSON') {
       <br />
       <a href="{{ getEmployerFecUrl() }}" target="_blank"
         >fec employee search</a
       >
+      } @if (entity().website) {
+      <br />
+      <a href="{{ entity().website }}" target="_blank">
+        {{ entity().name }} website
+      </a>
       }
       <p>{{ entity().summary }}</p>
       <p>last updated: {{ entity().updated_at }}</p>
       <a href="{{ entity().link }}" target="_blank">source</a>
+      <br />
+      <a
+        href="https://littlesis.org/api/entities/{{ entity().id }}"
+        target="_blank"
+        >data</a
+      >
     </div>
   `,
   styleUrl: './details-tab.css',

56b2e1800194231374341044a533710a3c910ad9
joshuaism <joshuaism@msn.com>
2025-06-23 08:56:25 +0900

    Add more outside links to entity details tab

--- src/app/details-tab/details-tab.ts
+++ src/app/details-tab/details-tab.ts
@@ -5,51 +5,7 @@ import { LittleSisNetwork } from '../network';
 @Component({
   selector: 'app-details-tab',
   imports: [],
-  template: `
-    <h1
-      style="cursor:pointer;"
-      title="center map on {{ entity().name }}"
-      (click)="focus(entity().id)"
-    >
-      {{ entity().name }}
-    </h1>
-    <h4>{{ entity().blurb }}</h4>
-    <div class="scrollable">
-      @if(entity().aliases && entity().aliases.length > 0) {
-      <h4>
-        aka: @for(alias of entity().aliases; track alias) {
-        <span>{{ alias }}</span>
-        @if(!$last) {<span>, </span>} }
-      </h4>
-      }
-      <a href="{{ getFecUrl() }}" target="_blank">fec search</a>
-      @if (entity().types[0].toUpperCase() !== 'PERSON') {
-      <br />
-      <a href="{{ getEmployerFecUrl() }}" target="_blank"
-        >fec employee search</a
-      >
-      } @if (entity().website) {
-      <br />
-      <a href="{{ entity().website }}" target="_blank">
-        {{ entity().name }} website
-      </a>
-      }
-      <p>{{ entity().summary }}</p>
-      <h4>
-        @for (type of entity().types; track type) {
-        {{ type }}<br />
-        }
-      </h4>
-      <p>last updated: {{ entity().updated_at }}</p>
-      <a href="{{ entity().link }}" target="_blank">source</a>
-      <br />
-      <a
-        href="https://littlesis.org/api/entities/{{ entity().id }}"
-        target="_blank"
-        >data</a
-      >
-    </div>
-  `,
+  templateUrl: './details-tab.html',
   styleUrl: './details-tab.css',
 })
 export class DetailsTab {
